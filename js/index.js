e147aa76b6bb22f5f464aea8b189c0c45b9b2085
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-04-24 21:17:24 -0300

    Adding info about items

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
-const acceptButtons = [];
+const infoButtons = [];
+const darkCurtain = document.getElementById('dark-curtain');
+let infoContainer;
+
 for (let i = 0; i < 6; i++) {
-    acceptButtons[i] = document.getElementById(`accept-button-${i+1}`);
+    infoButtons[i] = document.getElementById(`info-button-${i+1}`);
 }
- 
-for (let i of acceptButtons) {
-    i.addEventListener('click', async () => {
-        i.innerText = 'Adicionado!';
-        i.style.backgroundColor = '#22e032';
-        setTimeout(() => {
-            i.innerHTML = 'Carrinho';
-            i.style.backgroundColor = '#a2b9bc';
-        }, 2000)
+
+
+for (let i = 0; i < 6; i++) {
+    infoButtons[i].addEventListener('click', async () => {
+        infoContainer = document.getElementById(`info-${i+1}`)
+        infoContainer.style.display = 'flex';
+        darkCurtain.style.display = 'block';
     })
+}
+
+darkCurtain.addEventListener('click', () => {
+    infoContainer.style.display = 'none';
+    darkCurtain.style.display = 'none';
+})
+
+function carrinho(element) {
+    element.innerText = 'Adicionado!';
+    element.style.backgroundColor = '#22e032';
+    setTimeout(() => {
+        element.innerHTML = 'Carrinho';
+        element.style.backgroundColor = '#a2b9bc';
+    }, 2000)
 }
